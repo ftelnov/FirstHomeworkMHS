c366635a5ead9deeab12c26c49a08b6f6fa0be00
ftelnov <fntelnovfn@gmail.com>
2020-07-07 19:32:33 +0300

    Final version. Fixed some lint problems with backup, layout works properly

--- app/src/main/java/com/example/firsthomeworkmhs/MainActivity.kt
+++ app/src/main/java/com/example/firsthomeworkmhs/MainActivity.kt
@@ -5,11 +5,23 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.skill_item.*
 import ru.nobird.android.ui.adapters.DefaultDelegateAdapter
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var skillsAdapter: DefaultDelegateAdapter<Skill>
+    private var skills: ArrayList<Skill> = arrayListOf(
+        Skill("Kotlin", "~1y"),
+        Skill("Java", "~1.5y"),
+        Skill("Си/C++", "~3y"),
+        Skill("Lua", "0.5y"),
+        Skill("Js+html5+scss", "1.5y"),
+        Skill("Python", "3y")
+    )
+    private var skillsAdapter = ListDelegationAdapter(skillsAdapterDelegate())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,11 +30,18 @@ class MainActivity : AppCompatActivity() {
             openURL.data = Uri.parse("https://github.com/ftelnov")
             startActivityForResult(openURL, 1)
         }
-        skillsAdapter = DefaultDelegateAdapter()
-        skillsAdapter += SkillDataAdapterDelegate()
+        skillsAdapter.items = skills
         with(skillsContainer) {
             adapter = skillsAdapter
             layoutManager = LinearLayoutManager(context)
         }
     }
+
+    private fun skillsAdapterDelegate() =
+        adapterDelegateLayoutContainer<Skill, Skill>(R.layout.skill_item) {
+            bind {
+                skillName.text = item.name
+                exp.text = item.exp
+            }
+        }
 }
