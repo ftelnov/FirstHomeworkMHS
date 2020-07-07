041ffe51c1a1e8606bad7fc002c6e96f1664e73b
ftelnov <fntelnovfn@gmail.com>
2020-07-07 17:14:20 +0300

    Upper part is ready

--- app/src/main/java/com/example/firsthomeworkmhs/MainActivity.kt
+++ app/src/main/java/com/example/firsthomeworkmhs/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.firsthomeworkmhs
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toGithub.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://github.com/ftelnov")
+            startActivityForResult(openURL, 1)
+        }
     }
 }
