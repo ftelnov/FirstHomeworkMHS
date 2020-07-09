f92ad8ba0139432ab428a4a15ae021faa07abffc
ftelnov <fntelnovfn@gmail.com>
2020-07-09 15:16:16 +0300

    Добавил уникальные элементы из первого этапа во фрагмент. Теперь фильтрация происходит на уровне генерации фрагмента. Осталось довести до ума взаимодействия чекбоксов между собой.

--- app/src/main/java/com/example/firsthomeworkmhs/FilterFragment.kt
+++ app/src/main/java/com/example/firsthomeworkmhs/FilterFragment.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.RadioGroup
+import androidx.core.view.get
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.filter_item.*
+import kotlinx.android.synthetic.main.filter_item.view.*
 import kotlinx.android.synthetic.main.fragment_filter.*
+import kotlinx.android.synthetic.main.skill_item.*
 import java.io.Serializable
 
 class FilterFragment : Fragment() {
     private lateinit var skills: ArrayList<Skill>
     private lateinit var parent: FiltersParentActivity
+    private var filterDict: MutableMap<String, Boolean> = mutableMapOf()
+    private lateinit var checkBoxAll: CheckBox
+    private val filterAdapter = ListDelegationAdapter(filtersAdapterDelegate())
+    private lateinit var allCheckboxSelected: CompoundButton.OnCheckedChangeListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             skills = it.getSerializable("skills") as ArrayList<Skill>
+        }
+        for (item in skills) {
+            if (!filterDict.containsKey(item.exp)) {
+                filterDict[item.exp] = false
+            }
         }
     }
 
@@ -33,7 +52,37 @@ class FilterFragment : Fragment() {
         closeButton.setOnClickListener {
             parent.filterFragmentResulted(0, null)
         }
+        accept.setOnClickListener {
+            if (checkAll.isChecked) {
+                parent.filterFragmentResulted(0, null)
+            }
+        }
+        allCheckboxSelected = CompoundButton.OnCheckedChangeListener { _, isChecked ->
+            for (i in 0 until filterAdapter.itemCount) {
+                checkBoxes[i].filterCheck.isChecked = isChecked
+            }
+        }
+        checkBoxAll = checkAll
+        checkBoxAll.setOnCheckedChangeListener(allCheckboxSelected)
+        filterAdapter.items = filterDict.keys.toList()
+        with(checkBoxes) {
+            adapter = filterAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
+
+    private fun filtersAdapterDelegate() =
+        adapterDelegateLayoutContainer<String, String>(R.layout.filter_item) {
+            bind {
+                filterCheck.text = item
+                filterCheck.setOnCheckedChangeListener { _, isChecked ->
+                    filterDict[item] = isChecked
+                    if (!isChecked) {
+                        checkBoxAll.isChecked = false
+                    }
+                }
+            }
+        }
 
     companion object {
         @JvmStatic
